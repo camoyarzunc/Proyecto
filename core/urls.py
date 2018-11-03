1ae6c699353c202696dedcd7db38eef7364510ec
stobars <42249918+stobars@users.noreply.github.com>
2018-11-02 23:59:18 -0300

    'version2'

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from .views import index
-urlpatterns = [
-   path('', index,name="index")
-    
+from .views import index,formulario,page
+urlpatterns = [ 
+   path('', index,name="index"),
+   path('formulario.html',formulario,name="formulario"),
+   path('page.html',page,name="page")
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
- 
+from .models import Region,Ciudad,Comuna,TipoUsuario,Estado,Mascota,Registro
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index (request):
     return render(request, 'index.html')
+
+def formulario(request):
+    return render(request,'formulario.html')
+
+def page(request):
+    return render(request,'page.html')
